dfd9c18b8618d5f59317cb276d398cd9ab609641
inesoya <ioyacasero@gmail.com>
2018-11-22 14:40:05 +0100

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -45,7 +45,7 @@ $(function() {
                                     "username": "incoming-webhook",
                                     "mrkdwn": true,
                                     "attachments": [{
-                                        "text": "event_id: " + event.event_id,
+                                        "text": "event_id: " + data.event_id,
                                         "fallback": "You have received a new message from io_triggers!",
                                         "color": attachments_color_green,
                                         "attachment_type": "default"
