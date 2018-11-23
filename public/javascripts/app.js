f085acf1e1cccb8e88e3a41559038f7f9fa57142
inesoya <ioyacasero@gmail.com>
2018-11-23 09:54:31 +0100

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -45,7 +45,7 @@ $(function() {
                                     "username": "incoming-webhook",
                                     "mrkdwn": true,
                                     "attachments": [{
-                                        "text":  event.body.event."com.adobe.mcloud.pipeline.pipelineMessage"."com.adobe.mcloud.protocol.trigger"."enrichments"."analyticsHitSummary"."dimensions"."eVar140"."data",
+                                        "text":  event.body.event,
                                         "fallback": "You have received a new message from io_triggers!",
                                         "color": attachments_color_green,
                                         "attachment_type": "default"
